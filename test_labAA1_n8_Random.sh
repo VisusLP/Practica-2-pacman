a61abad8519288c141c971baaf0673c20dd69076
franmira98 <franmira98@gmail.com>
2019-05-12 18:11:12 +0200

    RandomGhost

---
+++ test_labAA1_n8_Random.sh
@@ -0,0 +1,12 @@
+# Tests the effectiveness of training just one time in labAA1
+
+# Resets the qtable
+cp qtable-ini.txt qtable.txt &&
+# Trains in the map labAA1 for 8 game
+python busters.py -p QLearningAgent -k 1 -l labAA1 -n 8 -g RandomGhost &&
+# Sets epsilon and alpha to 0, and executes the 5 maps 10 times
+python busters.py -p QLearningAgent -k 1 -l labAA1 -n 10 -r 0 -e 0 -q -g RandomGhost &&
+python busters.py -p QLearningAgent -k 2 -l labAA2 -n 10 -r 0 -e 0 -q -g RandomGhost &&
+python busters.py -p QLearningAgent -k 3 -l labAA3 -n 10 -r 0 -e 0 -q -g RandomGhost &&
+python busters.py -p QLearningAgent -k 3 -l labAA4 -n 10 -r 0 -e 0 -q -g RandomGhost &&
+python busters.py -p QLearningAgent -k 3 -l labAA5 -n 10 -r 0 -e 0 -q -g RandomGhost
