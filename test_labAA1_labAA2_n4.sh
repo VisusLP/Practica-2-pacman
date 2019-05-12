5aafc862cf62989378c8a6a9caaf98407f13946f
franmira98 <franmira98@gmail.com>
2019-05-12 23:20:27 +0200

    scripts

---
+++ test_labAA1_labAA2_n4.sh
@@ -0,0 +1,13 @@
+# Tests the effectiveness of training just one time in labAA1, labAA2
+
+# Resets the qtable
+cp qtable-ini.txt qtable.txt &&
+# Trains in the map labAA1, labAA2 for 4 game
+python busters.py -p QLearningAgent -k 1 -l labAA1 -n 4 &&
+python busters.py -p QLearningAgent -k 1 -l labAA2 -n 4 &&
+# Sets epsilon and alpha to 0, and executes the 5 maps 10 times
+python busters.py -p QLearningAgent -k 1 -l labAA1 -n 10 -r 0 -e 0 -q &&
+python busters.py -p QLearningAgent -k 2 -l labAA2 -n 10 -r 0 -e 0 -q &&
+python busters.py -p QLearningAgent -k 3 -l labAA3 -n 10 -r 0 -e 0 -q &&
+python busters.py -p QLearningAgent -k 3 -l labAA4 -n 10 -r 0 -e 0 -q &&
+python busters.py -p QLearningAgent -k 3 -l labAA5 -n 10 -r 0 -e 0 -q
---
+++ test_labAA1_n7_Random_and_static.sh
@@ -0,0 +1,12 @@
+# Tests the effectiveness of training just one time in labAA1
+
+# Resets the qtable
+cp qtable-ini.txt qtable.txt &&
+# Trains in the map labAA1 for 7 game
+python busters.py -p QLearningAgent -k 1 -l labAA1 -n 7 -g RandomGhost &&
+# Sets epsilon and alpha to 0, and executes the 5 maps 10 times
+python busters.py -p QLearningAgent -k 1 -l labAA1 -n 10 -r 0 -e 0 -q &&
+python busters.py -p QLearningAgent -k 2 -l labAA2 -n 10 -r 0 -e 0 -q &&
+python busters.py -p QLearningAgent -k 3 -l labAA3 -n 10 -r 0 -e 0 -q &&
+python busters.py -p QLearningAgent -k 3 -l labAA4 -n 10 -r 0 -e 0 -q &&
+python busters.py -p QLearningAgent -k 3 -l labAA5 -n 10 -r 0 -e 0 -q
