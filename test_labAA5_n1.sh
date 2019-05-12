b1eac591240d5ef7c1b9a10f1a07b5f45d681dbf
franmira98 <franmira98@gmail.com>
2019-05-12 17:50:02 +0200

    prueba mapa5

---
+++ test_labAA5_n1.sh
@@ -0,0 +1,12 @@
+# Tests the effectiveness of training just one time in labAA5
+
+# Resets the qtable
+cp qtable-ini.txt qtable.txt &&
+# Trains in the map labAA5 for 1 game
+python busters.py -p QLearningAgent -k 3 -l labAA5 -n 1 &&
+# Sets epsilon and alpha to 0, and executes the 5 maps 10 times
+python busters.py -p QLearningAgent -k 1 -l labAA1 -n 10 -r 0 -e 0 -q &&
+python busters.py -p QLearningAgent -k 2 -l labAA2 -n 10 -r 0 -e 0 -q &&
+python busters.py -p QLearningAgent -k 3 -l labAA3 -n 10 -r 0 -e 0 -q &&
+python busters.py -p QLearningAgent -k 3 -l labAA4 -n 10 -r 0 -e 0 -q &&
+python busters.py -p QLearningAgent -k 3 -l labAA5 -n 10 -r 0 -e 0 -q
